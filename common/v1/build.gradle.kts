e611e271dec50937fe60014da8d5c655ec0993bc
madhead <siarhei.krukau@gmail.com>
2022-01-16 02:27:10 +0100

    Fix #2: Move to GitHub Actions

--- common/v1/build.gradle.kts
+++ common/v1/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
     // By default, the code is compiled against the lowest supported AWS SDK versions.
-    // If BY_DEV_MADHEAD_AWS_JUNIT5_USE_LATEST_AWS_SDK environment variable is set to true we want to test the
+    // If ME_MADHEAD_AWS_JUNIT5_USE_LATEST_AWS_SDK environment variable is set to true we want to test the
     // code against the latest available AWS SDK versions.
-    val v1: String = if (System.getenv("BY_DEV_MADHEAD_AWS_JUNIT5_USE_LATEST_AWS_SDK") == "true") {
+    val v1: String = if (System.getenv("ME_MADHEAD_AWS_JUNIT5_USE_LATEST_AWS_SDK") == "true") {
         "1.+"
     } else {
         "1.11.79"
